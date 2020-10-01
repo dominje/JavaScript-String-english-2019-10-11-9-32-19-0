40f96caa64369fe767b6f5da436857e06cb3f1e9
JERICAH DOMINGO (SHP-IRIS-ISD-OOCLL/MNL) <DOMINJE@OOCL.COM>
2020-10-01 08:10:36 +0800

    Added answers to index.js

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+<!DOCTYPE html>
+<html>
+<head>
+<script>
 // Complete the following procedure, the 'name' variable in all letters to uppercase, output: ' 'HELLO' '.
 var name = 'hello';
+name = name.toUpperCase();
+alert(name);
 // TODO write your code here...
-
 
 // Complete the following procedure, capitalizing all the words in the variable sentence and printing: 'Good Afternoon, Mr Mike.'
 var sentence = 'good afternoon, mr mike.';
 //TODO write your code here...
+function capitalize(str) {
+  return str.replace(
+    /\w\S*/g,
+    function(txt) {
+      return txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase();
+    }
+  );
+}
+
+alert(capitalize(sentence));
 
 
 // Complete the following procedure to extract the amount of money variable, output: 20.
 var money = '￥20';
+res = money.substring(1, 3);
+alert(res);
+
 //TODO write your code here...
+
+</script>
+<title>Page Title</title>
+</head>
+<body>
+
+</body>
+</html>
